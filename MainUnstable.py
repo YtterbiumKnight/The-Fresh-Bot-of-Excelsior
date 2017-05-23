8d1fb81269a5cc75408fc47fbd982eaa06dfbc9e
Exanimem <thegamerZombiex100@gmail.com>
2017-05-23 17:55:24 -0400

    Began work on various features and small tweaks
    Included are awwnime feed and news articles not showing up twice, neither currently working. Some command names changed and rather than checking for my ID in [setlog, it now checks for the owner ID.

--- MainUnstable.py
+++ MainUnstable.py
@@ -1,4 +1,5 @@
 #The Fresh Bot of Excelsior by Exanimem#3112 and Ned#5609
+#test
 import discord
 import asyncio
 import logging
@@ -25,31 +26,41 @@ async def on_message(message): #Performs on message
     if message.author.id == client.user.id: #If message author is client
         return #Ignore
 
+    if message.author.id != '95978401654919168':
+        return
+
 #rss commands
 
     elif message.content.startswith("[rss"):
         feed = message.content[4:].strip()
         a = feedparser.parse(feed).entries[1]
-        await client.send_message(message.channel, '__***{0}***__ \n{1}'.format(a.title, a.link))
+        await client.send_message(message.channel, '***{0}*** \n{1}'.format(a.title, a.link))
 
     elif message.content.startswith("[topnews"):
         a = feedparser.parse('http://feeds.reuters.com/reuters/topNews').entries[1] #Gets top entry from Reuters Top News RSS feed
-        await client.send_message(message.channel, '__***{0}***__ \n{1}'.format(a.title, a.link)) #Formats message. Posts title, a line break then the link
+        await client.send_message(message.channel, '***{0}*** \n{1}'.format(a.title, a.link)) #Formats message. Posts title, a line break then the link
 
-    elif message.content.startswith("[randnews"):
-        r = random.randint(1, 10) #Picks a random number between 1 and 10
-        a = feedparser.parse('http://feeds.reuters.com/reuters/topNews').entries[r] #Gets a article in the order corresponding to the number randomly chosen from Reuters Top News RSS Feed
-        await client.send_message(message.channel, '__***{0}***__ \n{1}'.format(a.title, a.link)) #Formats message. Posts title, a line break then the link
+    elif message.content.startswith("[news"):
+        r = list(range(1, 10))
+        rv = random.choice(r)
+        r.remove(rv)
+        a = feedparser.parse('http://feeds.reuters.com/reuters/topNews').entries[rv] #Gets a article in the order corresponding to the number randomly chosen from Reuters Top News RSS Feed
+        await client.send_message(message.channel, '***{0}*** \n{1}'.format(a.title, a.link)) #Formats message. Posts title, a line break then the link
+
 
     elif message.content.startswith("[onion"):
         a = feedparser.parse('http://www.theonion.com/feeds/rss').entries[1] #Gets latest entry from Onion RSS feed
-        await client.send_message(message.channel, '__***{0}***__ \n{1}'.format(a.title, a.link)) #Formats message. Posts title, a line break then the link
+        await client.send_message(message.channel, '***{0}*** \n{1}'.format(a.title, a.link)) #Formats message. Posts title, a line break then the link
+
+    elif message.content.startswith("[awwnime"):
+        a = feedparser.parse('https://www.reddit.com/r/awwnime/.rss').entries[1] #Gets latest entry from Onion RSS feed
+        await client.send_message(message.channel, '***{0}*** \n{1}'.format(a.title, a.link.url)) #Formats message. Posts title, a line break then the link
 
 #rss commands end
 
     elif message.content.startswith("[setlog"): #Takes channel ID to post log messages. To Do: Save channel ID set for server
-        if message.author.id != '95978401654919168': #If author isn't Exanimem. To Do: Make this customizable so the owner can choose who can use this command
-            await client.send_message(message.channel, 'Sorry, certain bot commands are locked down for everyone except Exanimem due to exploits that need to be fixed')
+        if message.author.id != message.server.owner.id: #If not server owner
+            await client.send_message(message.channel, 'This command is only for the server owner and whoever they allow')
             return
         global log_channel #Makes log_channel usable in other functions
         log_channel = message.content[4:].strip()
@@ -57,7 +68,7 @@ async def on_message(message): #Performs on message
         return log_channel
 
     elif message.content.startswith("[say"): #Log who said what
-        if message.author.id != '95978401654919168': #Temporary until pings can be disabled
+        if message.author.id != message.server.owner.id: #Temporary until pings can be disabled
             await client.send_message(message.channel, 'Sorry, certain bot commands are locked down for everyone except Exanimem due to exploits that need to be fixed')
             return
         say = message.content[4:].strip() #Cuts the first four characters of the content
@@ -85,6 +96,6 @@ async def change_presence(game=None, idle=False): #Sets playing status. Doesn't 
 @client.event
 async def on_member_update(before, after): #When a parameter of member updates. Doesn't currently work. Wanted result: Check if change is addition of active role, post message down below in #lounge
     if before.roles != after.roles: #If before roles is different than after roles
-        for role in member.roles:
+        for roles in member.roles:
             if after.roles == "315622124494520320": #If after role is the active role
                 await client.send_messsage(discord.Object(id='283294159685681152'), '{} became Active! Enjoy your time here where we drink fine wine and smoke ripe Cuban cigars'.format()) #Sends message in #lounge
